18233c548e2313c3c9f895c53a3ed82fcd678a3c
osariemenro <osariemenro@gmail.com>
2022-10-12 00:25:10 +0100

    Quote generator

--- scripts.js
+++ scripts.js
@@ -3,8 +3,23 @@ const quoteText = document.getElementById("quote")
 const authorText = document.getElementById("author")
 const twitterBtn = document.getElementById("twitter")
 const newQuoteBtn = document.getElementById("new__quote")
+const loader = document.getElementById("loader")
+
+// Show loading
+const loading = () => {
+	loader.hidden = false
+	quoteContainer.hidden = true
+}
+
+const complete = () => {
+	if(!loader.hidden) {
+		quoteContainer.hidden = false
+		loader.hidden = true
+	}
+}
 
 async function getQuotes() {
+	loading()
 	const apiUrl = "https://my-json-server.typicode.com/osariemenro/quote-generator/db"
 
 	try {
@@ -12,6 +27,7 @@ async function getQuotes() {
 		const data = await response.json()
 
 		renderData(data)
+		complete()
 	} catch (error) {
 		console.log(error)
 	}
@@ -21,14 +37,35 @@ const renderData = (data) => {
 	const random = Math.floor((data.quotes.length * Math.random()))
 	const quotes = data.quotes[random]
 
-	quoteText.textContent = quotes.quote
-
+	// if author is blank
 	if(quotes.author === "") {
 		authorText.textContent = "Unknown"
 	}
 	else {
 		authorText.textContent = quotes.author
 	}
+
+	// reduce font size for characters greeter than 120
+	if(quotes.quote.length > 30) {
+		authorText.classList.add("long__quote")
+	}
+	else {
+		authorText.classList.remove("long__quote")
+	}
+
+	quoteText.textContent = quotes.quote
 }
+
+// Tweet quote
+const tweetQuote = () => {
+	const quote = quoteText.textContent
+	const author = authorText.textContent
+	
+	const twitterUrl = `https://www.twitter.com/intent/tweet/?text=${quote} - ${author}`
+	window.open(twitterUrl, '_blank')
+}
+
+newQuoteBtn.addEventListener("click", getQuotes)
+twitterBtn.addEventListener("click", tweetQuote)
 
 getQuotes()
