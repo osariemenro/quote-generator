e778e65fd6feb50ef2db775478138c36179aaf68
osariemenro <osariemenro@gmail.com>
2022-10-11 23:46:42 +0100

    Quote loads to home

--- scripts.js
+++ scripts.js
@@ -1,13 +1,34 @@
-// Get Quote From API
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '7e39ddc964msh60f9aee978e52c8p10d531jsn24622d8b22f5',
-		'X-RapidAPI-Host': 'theysaidso.p.rapidapi.com'
-	}
-};
+const quoteContainer = document.getElementById("quote__container")
+const quoteText = document.getElementById("quote")
+const authorText = document.getElementById("author")
+const twitterBtn = document.getElementById("twitter")
+const newQuoteBtn = document.getElementById("new__quote")
 
-fetch('https://theysaidso.p.rapidapi.com/quote/random?language=en', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+async function getQuotes() {
+	const apiUrl = "https://my-json-server.typicode.com/osariemenro/quote-generator/db"
+
+	try {
+		const response = await fetch(apiUrl)
+		const data = await response.json()
+
+		renderData(data)
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+const renderData = (data) => {
+	const random = Math.floor((data.quotes.length * Math.random()))
+	const quotes = data.quotes[random]
+
+	quoteText.textContent = quotes.quote
+
+	if(quotes.author === "") {
+		authorText.textContent = "Unknown"
+	}
+	else {
+		authorText.textContent = quotes.author
+	}
+}
+
+getQuotes()
